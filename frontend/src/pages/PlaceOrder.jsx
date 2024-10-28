df0c43cad846bced9c2261b477fe9b607cb4d4f2
imihirjain <jmanav251@gmail.com>
2024-10-29 00:08:45 +0530

    complete navbar with small screen

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 
 const PlaceOrder = () => {
-  return <div></div>;
+  return <div>PlaceOrder</div>;
 };
 
 export default PlaceOrder;
