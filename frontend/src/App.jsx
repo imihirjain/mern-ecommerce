2ca9ba62f44476f99a8952874b45c618f662a7c1
imihirjain <jmanav251@gmail.com>
2024-10-28 23:00:10 +0530

    create pages and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import Orders from "./pages/Orders";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
